dbf2bce282a78aa517bf1b97d8811ef2b23b20b0
PatrekGill <patrekGill@yahoo.com>
2022-04-12 17:42:50 -0600

    added basic apollo server setup with GraphQL

--- api/index.js
+++ api/index.js
@@ -11,32 +11,64 @@ const typeDefs = gql`
         title: String
         author: String
     }
+	type Game {
+		title: String
+	}
+	input GameInput {
+		title: String
+	}
 
     # The "Query" type is special: it lists all of the available queries that
     # clients can execute, along with the return type for each. In this
     # case, the "books" query returns an array of zero or more Books (defined above).
     type Query {
         books: [Book]
+		games: [Game]
     }
+
+	type Mutation {
+		addGame(game: GameInput): [Game]
+	}
 `;
 
-const books = [
-    {
-        title: "The Awakening",
-        author: "Kate Chopin",
-    },
-    {
-        title: "City of Glass",
-        author: "Paul Auster",
-    },
-];
+const fakeDatabase = {
+	books: [
+		{
+			title: "The Awakening",
+			author: "Kate Chopin",
+		},
+		{
+			title: "City of Glass",
+			author: "Paul Auster",
+		},
+	],
+	games: [
+		{
+			title: "Mass Effect",
+		},
+		{
+			title: "Mass Effect 2",
+		},
+		{
+			title: "Mass Effect 3",
+		},
+	]
+};
+
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-        books: () => books,
+        books: () => fakeDatabase.books,
+        games: () => fakeDatabase.games,
     },
+	Mutation: {
+		addGame: (root,args,context,info) => {
+			fakeDatabase.games.push(args.game);
+			return fakeDatabase.games;
+		},
+	}
 };
 
 // The ApolloServer constructor requires two parameters: your schema
