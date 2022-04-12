f7e5caa5effd35b173f26b3ed9e0a4c2e6d0c909
PatrekGill <patrekGill@yahoo.com>
2022-04-12 09:58:23 -0600

    added firebase to ui

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'my-game-list-41bd5',
+    appId: '1:348620035394:web:c1816f5330cd4003ce24db',
+    storageBucket: 'my-game-list-41bd5.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBHSuxN2QUTQg0fqXccvX3aMRGNbbv9C38',
+    authDomain: 'my-game-list-41bd5.firebaseapp.com',
+    messagingSenderId: '348620035394',
+    measurementId: 'G-MH7E978VR5',
+  },
   production: false
 };
 
