059cb8181f93f32039e3c1ab7e85f973a2754dc2
PatrekGill <patrekGill@yahoo.com>
2022-05-15 09:52:26 -0600

    Moved firebase admin app init to index

--- api/functions/src/index.ts
+++ api/functions/src/index.ts
@@ -1,7 +1,8 @@
 import * as functions from "firebase-functions";
-// import serviceAccount from "../../service-account.js";
+import * as admin from "firebase-admin";
+import {getFirestore} from "firebase-admin/firestore";
 import { ApolloServer } from "apollo-server-cloud-functions";
-import * as schema from "./schema/schema";
+const {getSchema} = require("./schema/schema");
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -26,26 +27,32 @@ exports.graphql = async function (event, context, next) {
     return apolloHandler(event, context, next);
 };
 */
+const main = () => {
+	process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
+	const serviceAccount = require("../private/service-account.json");
+	const app = admin.initializeApp({
+		credential: admin.credential.cert(serviceAccount),
+		databaseURL: "https://my-game-list-41bd5.firebaseio.com",
+	});
+	
+	const schema = getSchema(app);
+	const server = new ApolloServer(schema);
+	const cors = {
+		origin: true,
+		credentials: true,
+	};
+	
+	const apolloHandler = server.createHandler({
+		expressGetMiddlewareOptions: { cors },
+	});
 
-
-const server = new ApolloServer(schema);
-const cors = {
-    origin: true,
-    credentials: true,
-};
-
-const apolloHandler = server.createHandler({
-    expressGetMiddlewareOptions: { cors },
-});
-
-exports.graphql = functions.https.onRequest(
-	(req,resp) => {
-    	return apolloHandler(req,resp,() => {});
+	module.exports = {
+		graphql: functions.https.onRequest(
+			(req,resp) => {
+				return apolloHandler(req,resp,() => {});
+			}
+		),
 	}
-);
+}
 
-
-/*
-const server = new ApolloServer(schema);
-exports.handler = server.createHandler();
-*/
+main();
--- api/functions/src/schema/schema.ts
+++ api/functions/src/schema/schema.ts
@@ -1,17 +1,10 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import { gql } from "apollo-server";
 import * as admin from "firebase-admin";
-import {getFirestore} from "firebase-admin/firestore";
+import { gql } from "apollo-server";
 import * as logger from "firebase-functions/logger";
+import { getFirestore } from "firebase-admin/firestore";
 
-process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
-const serviceAccount = require("../../private/service-account.json");
-const app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://my-game-list-41bd5.firebaseio.com",
-});
 
-const firestore = getFirestore(app);
 // const games = [
 //     {
 //         id: 1,
@@ -34,15 +27,6 @@ interface Game {
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
-const typeDefs = gql`
-    type Game {
-        id: Int
-        title: String
-    }
-    type Query {
-        games: [Game]
-    }
-`;
 
 // const games = async () => {
 //     const games = await admin.firestore().collection("games").get();
@@ -51,27 +35,47 @@ const typeDefs = gql`
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
-const resolvers = {
-    Query: {
-        // games: () => games,
-		games: async () => {
-			const collection = firestore.collection("games").get();
-			return (await collection).docs.map(game => game.data());
-		}	
-    },
-};
+
+const getSchema = (app: admin.app.App) => {
+	const firestore = getFirestore(app);
+	
+	const typeDefs = gql`
+	    type Game {
+	        id: Int
+	        title: String
+	    }
+	    type Query {
+	        games: [Game]
+	    }
+	`;
+
+
+	const resolvers = {
+		Query: {
+			// games: () => games,
+			games: async () => {
+				const collection = firestore.collection("games").get();
+				return (await collection).docs.map(game => game.data());
+			}	
+		},
+	};
+
+	return {
+		schema: makeExecutableSchema({
+			typeDefs,
+			resolvers,
+		}),
+		// Bonus function, to remove stack trace on production
+		// https://www.apollographql.com/docs/apollo-server/features/errors
+		formatError: (error: any) => {
+			if (process.env.NODE_ENV === "production") {
+				delete error.extensions.exception;
+			}
+			return error;
+		},
+	}
+}
 
 module.exports = {
-    schema: makeExecutableSchema({
-        typeDefs,
-        resolvers,
-    }),
-    // Bonus function, to remove stack trace on production
-    // https://www.apollographql.com/docs/apollo-server/features/errors
-    formatError: (error: any) => {
-        if (process.env.NODE_ENV === "production") {
-            delete error.extensions.exception;
-        }
-        return error;
-    },
+	getSchema
 };
